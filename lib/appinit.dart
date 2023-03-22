367424d0ae8aa87f3ae693d9302d68bf5eb7b306
M. Sertaç Toröz <msertactoroz@gmail.com>
2023-03-22 19:23:53 +0300

    lottie animation created

--- lib/appinit.dart
+++ lib/appinit.dart
@@ -2,6 +2,7 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:kalorientracker/view/pages/home_page.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:lottie/lottie.dart';
 
 class AppInit extends StatelessWidget {
   const AppInit({super.key});
