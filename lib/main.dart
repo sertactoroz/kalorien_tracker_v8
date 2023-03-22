86d3ebcfd1d12864da203d8e4454112fefcc6d2e
M. Sertaç Toröz <msertactoroz@gmail.com>
2023-03-22 14:55:15 +0300

    bottom navbar settled

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:model_test/pages/login/login_page.dart';
-// import 'package:model_test/view_models/profile_view_model.dart';
-import 'package:provider/provider.dart';
-import 'dart:convert';
-// import 'pages/profile/profile_page.dart';
-import 'models/user_model.dart';
-import 'view/home_page.dart';
-// import 'view_models/login_view_model.dart';
+import 'package:kalorientracker/view/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,15 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage(),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
