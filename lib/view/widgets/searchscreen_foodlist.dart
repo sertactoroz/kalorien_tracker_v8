ae9eb12a6dd1442363243d42b03611fc60bf8f02
Ömer İsmailoğlu <farukismailoglu0@gmail.com>
2023-03-24 05:04:15 +0300

    searchscreen_foodlist added

---
+++ lib/view/widgets/searchscreen_foodlist.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class FoodList extends StatefulWidget {
+  const FoodList({super.key});
+
+  @override
+  State<FoodList> createState() => _FoodListState();
+}
+
+class _FoodListState extends State<FoodList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            width: MediaQuery.of(context).size.width,
+            height: 80,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color.fromARGB(97, 151, 150, 150)),
+            child: Row(
+              children: [
+                Text("Yemek"),
+                Spacer(),
+                TextButton(onPressed: () {}, child: Text("Add")),
+                TextButton(onPressed: () {}, child: Text("Update"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
